5383cf665be09299859dc74970d0f40114a035cd
Zero-user <77414062+Zero3720@users.noreply.github.com>
2025-11-17 18:51:31 -0300

    Atualiza a inicialização do FastAPI e a criação do banco de dados no main.py (Eu acho)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from src.Controllers import authController, userController
+from Database.database import Base, engine
+
 
 app = FastAPI()
-
+Base.metadata.create_all(bind=engine)
 app.include_router(authController.router)
 app.include_router(userController.router)
